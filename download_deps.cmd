4416e148ed344becd385d59c9065bac23c714ec2
Kramoule <22647002+Kramoule@users.noreply.github.com>
2026-06-16 00:40:55 +0200

    build: fix compatibility with GOG Galaxy 2.1.3 onwards
    GOG GALAXY 2.1.3 now uses Python 3.13 and only works on 64-bits systems.
    
    galaxy.plugin.api has to be updated.
    
    Scripts are updated to target GALAXY system requirements:
    Windows 64bits and macOS 14+ (ARM and Intel).
    
    The cgi module was removed in Python 3.13. A fork named "legacy-cgi"
    was created as as a drop-in replacement.
    
    We use this opportunity to bump other modules.
    
    A dev requirement file is created to be able to call piptools from a
    virtual environment.

--- download_deps.cmd
+++ download_deps.cmd
@@ -1,4 +1,4 @@
 python -m piptools compile ./requirements.txt --output-file ./requirements_compiled.txt 
-python -m pip install -r ./requirements_compiled.txt --platform win32 --target ./3rdparty_windows --python-version 37 --no-compile --no-deps
-python -m pip install -r ./requirements_compiled.txt --platform macosx_10_13_x86_64 --target ./3rdparty_macos --python-version 37 --no-compile --no-deps
+python -m pip install -r ./requirements_compiled.txt --platform win_amd64 --target ./3rdparty_windows --python-version 313 --no-compile --no-deps
+python -m pip install -r ./requirements_compiled.txt --platform macosx_14_0_universal2 --target ./3rdparty_macos --python-version 313 --no-compile --no-deps
 del requirements_compiled.txt 
--- download_deps.sh
+++ download_deps.sh
@@ -1,4 +1,4 @@
 python3 -m piptools compile ./requirements.txt --output-file ./requirements_compiled.txt 
-python3 -m pip install -r ./requirements_compiled.txt --platform win32 --target ./3rdparty_windows --python-version 37 --no-compile --no-deps
-python3 -m pip install -r ./requirements_compiled.txt --platform macosx_10_13_x86_64 --target ./3rdparty_macos --python-version 37 --no-compile --no-deps
+python3 -m pip install -r ./requirements_compiled.txt --platform win_amd64 --target ./3rdparty_windows --python-version 313 --no-compile --no-deps
+python3 -m pip install -r ./requirements_compiled.txt --platform macosx_14_0_universal2 --target ./3rdparty_macos --python-version 313 --no-compile --no-deps
 rm ./requirements_compiled.txt 
